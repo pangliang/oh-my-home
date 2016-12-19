b84498e8f3272c173a77baeee590f54326f5cbd2
pangliang <418094911@qq.com>
2016-12-19 11:27:41 +0800

    linux 的apt-get 加上sudo

--- install.sh
+++ install.sh
@@ -12,8 +12,7 @@ fi
 
 
 # git
-
-apt-get install git
+sudo apt-get install git
 
 git config --global user.email "418094911@qq.com"
 git config --global user.name "pangliang"
@@ -27,7 +26,7 @@ git submodule foreach git pull origin master
 git submodule foreach git checkout master
 
 # vim
-apt-get install vim lua
+sudo apt-get install vim lua
 
 rm -rf ~/.vim ~/.vimrc
 
@@ -36,7 +35,7 @@ ln -s ~/.vim/vimrc ~/.vimrc
 vim +PluginInstall +qall
 
 # zsh
-apt-get install zsh
+sudo apt-get install zsh
 
 rm -rf ~/.oh-my-zsh ~/.zshrc
 
@@ -44,7 +43,7 @@ ln -s $PWD/oh-my-zsh ~/.oh-my-zsh
 cp -rf ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 # tmux
-apt-get install tmux
+sudo apt-get install tmux
 rm -rf ~/.tmux ~/.tmux.conf ~/.tmux.conf.local
 
 ln -s $PWD/oh-my-tmux ~/.tmux
