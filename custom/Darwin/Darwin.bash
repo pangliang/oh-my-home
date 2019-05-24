a3aff07e86da5e10f00597c302afb08d0adcadcc
pangliang <418094911@qq.com>
2019-05-24 15:01:06 +0800

    用skhd替换khd

--- custom/Darwin/Darwin.bash
+++ custom/Darwin/Darwin.bash
@@ -4,13 +4,13 @@ APT="brew"
 echo "install chunkwn ..."
 brew tap crisidev/homebrew-chunkwm
 brew install chunkwm
-brew install koekeishiya/formulae/khd
+brew install koekeishiya/formulae/skhd
 rm ~/.chunkwmrc
 ln -s $PWD/custom/Darwin/chunkwmrc ~/.chunkwmrc 
-rm ~/.khdrc
-ln -s $PWD/custom/Darwin/khdrc ~/.khdrc 
+rm ~/.skhdrc
+ln -s $PWD/custom/Darwin/skhdrc ~/.skhdrc 
 brew services restart crisidev/chunkwm/chunkwm
-brew services restart khd
+brew services restart skhd
 
 # copy to OS clipboard
 brew install reattach-to-user-namespace
