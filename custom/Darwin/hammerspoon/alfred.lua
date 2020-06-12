fb102bad8bca2acf7c13ff054e33072d1942efb0
pangliang <418094911@qq.com>
2020-06-12 15:35:13 +0800

    alfred好用了

--- custom/Darwin/hammerspoon/alfred.lua
+++ custom/Darwin/hammerspoon/alfred.lua
@@ -8,7 +8,31 @@ plugins.lock = {
     title = "电脑锁屏",
     completionCallback = function(value)
         log.f("lock: %s", hs.inspect(value))
-        --hs.caffeinate.lockScreen()
+        hs.caffeinate.lockScreen()
+    end
+}
+
+plugins.calc = {
+    title = "calc",
+    choices = function(query)
+        local choices = {}
+        if query == nil or query == "" then
+            return choices
+        end
+    
+        -- Filter out commas and dollar signs
+        query, _ = query:gsub("[%,%$]", "")
+    
+        -- We need to determine if the query only contains mathematical calculations
+        -- To do this we'll see if it matches the inverse of that set of characters
+        if string.match(query, "[^%d^%.^%+^%-^/^%*^%^^ ^%(^%)]") == nil then
+            local compile_result, fn = load("return " .. query)
+            if type(compile_result) == "function" then
+                local result = compile_result()
+                table.insert(choices, {text=result, value=result})
+            end
+        end
+        return choices
     end
 }
 
@@ -21,40 +45,39 @@ local chooser = hs.chooser.new(function(chosen)
     end
     local plugin = plugins[chosen.plugin]
     local arg = string.match(chosen.value, "^"..chosen.plugin.." (.+)")
-    if not plugin.argRequired or nil ~= arg then
+    if plugin.argRequired == false or nil ~= arg then
         plugin.completionCallback(arg)
     else
-        if plugin.choices then
-            alfred.chooser:queryChangedCallback()
-            local datas = plugin.choices("")
-            for i, d in ipairs(datas) do
-                d.plugin = chosen.plugin
-                d.value = string.format("%s %s", chosen.plugin, d.value)
+        alfred.chooser:queryChangedCallback(function (queryStr)
+            log.f("plugin.choices queryChangedCallback: %s", queryStr)
+            local choices = {}
+            if plugin.choices then
+                for i, d in ipairs(plugin.choices(queryStr)) do
+                    if nil ~= string.match(d.text, queryStr) or nil ~= string.match(d.subText, queryStr) then
+                        d.plugin = chosen.plugin
+                        d.value = string.format("%s %s", chosen.plugin, d.value)
+                        hs.fnutils.concat(choices, {d})
+                    end
+                end
             end
-            alfred.chooser:choices(datas)
-        else
-            alfred.chooser:queryChangedCallback(function (queryStr)
-                alfred.chooser:choices({{ text = plugin.title, plugin = chosen.plugin, value = chosen.plugin.." "..queryStr}})
-            end)
-        end
-
+            if #choices == 0 then
+                hs.fnutils.concat(choices, {{ text = plugin.title, subText = queryStr, plugin = chosen.plugin, value = string.format("%s %s", chosen.plugin, queryStr)}})
+            end
+            alfred.chooser:choices(choices)
+        end)
         alfred.chooser:show()
     end
 end)
-chooser:placeholderText("输入指令")
 chooser:searchSubText(true)
 
-hs.hotkey.bind({ 'ctrl' }, "g", function()
+hs.hotkey.bind({ 'ctrl' }, 'g', function()
+    chooser:placeholderText("输入指令")
     chooser:queryChangedCallback(function (queryStr)
         log.f("queryChangedCallback: %s", queryStr)
         local choices = {}
         for plugin_name, plugin in pairs(plugins) do
             if nil ~= string.match(plugin_name, "^"..queryStr) or nil ~= string.match(queryStr, "^".. plugin_name) then
-                local datas = plugin.choices and plugin.choices(queryStr) or { { text = plugin.title, subText = plugin.subText, plugin = plugin_name, value = queryStr}}
-                for i, d in ipairs(datas) do
-                    d.plugin = plugin_name
-                    d.value = string.format("%s %s", plugin_name, d.value)
-                end
+                local datas = {{ text = plugin.title, subText = plugin.subText, plugin = plugin_name, value = string.format("%s", queryStr)}}
                 hs.fnutils.concat(choices, datas)
             end
         end
