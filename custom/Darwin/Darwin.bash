06544f7a1f6f9d893bde474c485b2cacafe89144
pangliang <418094911@qq.com>
2020-06-12 15:34:50 +0800

    去掉chunkwmrc

--- custom/Darwin/Darwin.bash
+++ custom/Darwin/Darwin.bash
@@ -1,16 +1,5 @@
 
 APT="brew"
 
-echo "install chunkwn ..."
-brew tap crisidev/homebrew-chunkwm
-brew install chunkwm
-brew install koekeishiya/formulae/skhd
-rm ~/.chunkwmrc
-ln -s $PWD/custom/Darwin/yabairc ~/.yabairc 
-rm ~/.skhdrc
-ln -s $PWD/custom/Darwin/skhdrc ~/.skhdrc 
-brew services restart crisidev/chunkwm/chunkwm
-brew services restart skhd
-
 # copy to OS clipboard
 brew install reattach-to-user-namespace
